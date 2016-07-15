b35982eae17ae6bb7dc6b1d56a7d6e2522d68dda
lingtran <lxiutran@gmail.com>
2016-07-15 00:13:20 -0600

    refactor ruby hamming exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,19 +1,15 @@
 class Hamming
-  VERSION = 1
-  
+  VERSION = 2
+
   def self.compute(strand_one, strand_two)
-    raise ArgumentError unless Hamming.verify_equivalent_lengths(strand_one, strand_two)
+    verify_equivalent_lengths(strand_one, strand_two)
 
-    counter = 0
-
-    strand_one.chars.each_with_index do |element, index|
-      counter +=1 if strand_one[index] != strand_two[index]
+    (0..strand_one.length).count do |i|
+      strand_one[i] != strand_two[i]
     end
-
-    counter
   end
 
   def self.verify_equivalent_lengths(strand_one, strand_two)
-    strand_one.length == strand_two.length
+    raise ArgumentError unless strand_one.length == strand_two.length
   end
 end
