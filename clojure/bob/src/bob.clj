b59931aecf501d22936a64aa64abfb14c804bf33
lingtran <lxiutran@gmail.com>
2016-07-24 11:27:29 -0600

    complete bob clojure exercism

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,8 +1,22 @@
 (ns bob)
 
-(defn response-for [something]
-  (if (clojure.string/blank? something)
-    "Fine. Be that way!"
-    ( if (something (= "WHAT OUT!"))
-        "Whoa, chill out!"
-        "Whatever.")))
+(defn numbers? [message]
+  (not (re-find #"[A-Za-z]" message))
+)
+
+(defn shouting? [message]
+  (and ( = (clojure.string/upper-case message) message) (not (numbers? message)))
+)
+
+(defn question? [message]
+  (= (last message) \?)
+)
+
+(defn response-for [message]
+  (cond
+    (clojure.string/blank? message) "Fine. Be that way!"
+    (shouting? message) "Whoa, chill out!"
+    (question? message) "Sure."
+    :else "Whatever."
+  )
+)
