7279b212dc4b58b045be1cfc622eab9328a555da
lingtran <lxiutran@gmail.com>
2016-08-25 18:16:31 -0600

    pass tests for javascript bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,12 +3,36 @@
 // convenience to get you started writing code faster.
 //
 
-var Bob = function() {};
+var Bob = function() {
+  this.responseToSilence  = "Fine. Be that way!";
+  this.responseToYelling  = "Whoa, chill out!";
+  this.responseToQuestion = "Sure.";
+  this.responseInGeneral   = "Whatever.";
+
+};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if (checkForYelling(input)) {
+    return this.responseToYelling;
+  } else if (checkForQuestion(input)) {
+  return this.responseToQuestion;
+  } else if (checkForSilence(input)) {
+    return this.responseToSilence;
+  } else {
+    return this.responseInGeneral;
+  }
 };
+
+function checkForQuestion(input) {
+  return input.endsWith('?');
+}
+
+function checkForYelling(input) {
+  return input.toUpperCase() == input && input.toLowerCase() != input;
+}
+
+function checkForSilence(input) {
+  return /^\s*$/.test(input);
+}
 
 module.exports = Bob;
