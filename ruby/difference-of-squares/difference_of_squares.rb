93eb389a8ed9bf2c198e6169ef594338f4891e85
lingtran <lxiutran@gmail.com>
2016-08-26 16:51:21 -0600

    refactor implementation of diff of squares ruby solution

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,14 +1,8 @@
 class Squares
   attr_reader :collection
-  attr_accessor :difference
 
   def initialize(number)
-    if number != 0
-      @collection = (1..number).to_a
-      @difference = find_difference
-    else
-      @difference = 0
-    end
+    @collection = (0..number).to_a
   end
 
   def square_of_sum
@@ -19,11 +13,11 @@ class Squares
     collection.reduce(0) { |sum, num| sum += num**2 }
   end
 
-  def find_difference
-    @difference = square_of_sum - sum_of_squares
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
 
 module BookKeeping
-  VERSION = 3
+  VERSION = 4
 end
