9ed2c0b42853917a53926bc8a2d8cdbdaaefadce
lingtran <lxiutran@gmail.com>
2016-11-14 21:39:37 -0600

    Modify implementation of getting from LETTERS
    Introduce single responsibility methods to handle selection and removal of ltters from constant
    Still trying to figure out better implementation that does not involve changing constant

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class Robot
   LETTERS = ("A".."Z").to_a
-  attr_reader :name
+
+  attr_reader :name, :letters
 
   def initialize
     @name = generate_name
@@ -12,9 +13,18 @@ class Robot
     initialize
   end
 
-  def generate_name
-    "%s%s%03d" % [LETTERS.delete(LETTERS.sample), LETTERS.delete(LETTERS.sample), rand(999)]
+  def modify_letters_when(letter)
+    LETTERS.delete(letter)
   end
+
+  def get_letter
+    LETTERS.sample
+  end
+
+  def generate_name
+    "%s%s%03d" % [modify_letters_when(get_letter), modify_letters_when(get_letter), rand(999)]
+  end
+
 end
 
 class BookKeeping
